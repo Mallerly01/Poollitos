94c443461c00129dd50b1f3c2ba024ba72e6b0c3
valentina.eriza2201 <valentina.eriza2201@alumnos.ubiobio.cl>
2023-04-22 19:44:22 -0400

    clase cliente y producto

--- src/Modelo/Cliente.java
+++ src/Modelo/Cliente.java
@@ -1,4 +1,48 @@
 package Modelo;
 
 public class Cliente {
+    private String rut;
+    private String nombre;
+    private String direccion;
+    private String telefono;
+
+    public Cliente(String rut, String nom, String dir, String fono){
+        this.rut = rut;
+        nombre = nom;
+        direccion = dir;
+        telefono = fono;
+    }
+
+    public String getRut() {
+        return rut;
+    }
+
+    public void setRut(String rut) {
+        this.rut = rut;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
 }
--- src/Modelo/Producto.java
+++ src/Modelo/Producto.java
@@ -1,4 +1,48 @@
 package Modelo;
 
 public class Producto {
+
+    private long codigo;
+    private String marca;
+    private String descripcion;
+    private int precio;
+
+    public Producto(long code, String marc, String desc, int precio){
+        codigo = code;
+        marca = marc;
+        descripcion = desc;
+        this.precio = precio;
+    }
+
+    public long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(long codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
 }
