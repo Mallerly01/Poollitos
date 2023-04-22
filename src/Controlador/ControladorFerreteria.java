ac9dead3cc28b56305775bfb109be962e2acd5cd
Mallerly01 <mallerly.carrasco2201@alumnos.ubiobio.cl>
2023-04-22 19:38:23 -0400

    realizacion del controlador

--- src/Controlador/ControladorFerreteria.java
+++ src/Controlador/ControladorFerreteria.java
@@ -1,5 +1,42 @@
 package Controlador;
 
+import Modelo.*;
+
 public class ControladorFerreteria {
     private static ControladorFerreteria instance= null;
+    private static Cliente[] arrayClientes;
+    private int contClientes=0;
+    private static Producto[] arrayProductos;
+    private int contProductos=0;
+    private ControladorFerreteria(){
+        arrayClientes = new Cliente[100];
+        arrayProductos =  new Producto[100];
+    }
+    public static ControladorFerreteria getInstance() {
+        if (instance==null){
+            instance= new ControladorFerreteria();
+        }
+        return instance;
+    }
+
+    public void creaCliente(Cliente nuevo){
+        arrayClientes[contClientes]=nuevo;
+    }
+    public void creaProducto(Producto nuevo){
+        arrayProductos[contProductos]=nuevo;
+    }
+    public Cliente[] listaClientes(){
+        Cliente[] clientes = new Cliente[contClientes];
+        for (int i =0 ; i<contClientes;i++){
+            clientes[i]=arrayClientes[i];
+        }
+        return clientes;
+    }
+    public Producto[] listaProductos(){
+        Producto[] productos = new Producto[contProductos];
+        for (int i=0; i<contProductos;i++){
+            productos[i]=arrayProductos[i];
+        }
+        return productos;
+    }
 }
